06ab1169d873d2f3dd55e3debf55daffca37a2a3
ahmad shakik <ahmadshakik777@gmail.com>
2023-03-28 02:51:16 +0300

    adding speech

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello all!nice to meet you!</p>
+        <p>Hello all!nice to meet you!Hope you are doing well!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
